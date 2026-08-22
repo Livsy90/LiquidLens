3acbe327a2fb5a4e8d22c2cc62f2596f058bb9bc
Livsy90 <artmir90@gmail.com>
2026-08-22 22:04:58 +0400

    Initial Commit

---
+++ LiquidLens/LiquidLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiquidLensApp.swift
+//  LiquidLens
+//
+//  Created by Artem Mir on 22.08.26.
+//
+
+import SwiftUI
+
+@main
+struct LiquidLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
